caaaa79ac3592eb5013b9f03567971bd788ee1e5
amirboxer <amirboxer95@gmail.com>
2024-05-29 18:52:58 +0200

    Limit the ball size diameter to 400px

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 function onBallClick() {
     var ball = document.querySelector('.ball')
-    var ballDiameter = ball.clientHeight + 50
+    var ballDiameter = ball.clientHeight + 50 > 400 ? 100 : ball.clientHeight + 50
     ball.innerHTML = ballDiameter
     ball.style = `height: ${ballDiameter}px; width: ${ballDiameter}px;`
 }
