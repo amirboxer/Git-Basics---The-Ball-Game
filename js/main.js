b9b0a1a55ea1b67796c2cd211ea9e36e97180104
amirboxer <amirboxer95@gmail.com>
2024-05-30 14:45:58 +0200

    Second ball add

--- js/main.js
+++ js/main.js
@@ -1,14 +1,13 @@
 'use strict'
 
-function onBallClick(initialDiameter) {
-    var ball = document.querySelector('.ball')
-    var ballDiameter = +initialDiameter
-    ball.onclick = () => {
+function onBallClick(elBall) {
+    var ballDiameter = +elBall.innerHTML
+    elBall.onclick = () => {
         var incrementAmount = getRandomInt(20, 60)
         var newColor = getRandomColor()
         ballDiameter = ballDiameter + incrementAmount > 400 ? 100 : ballDiameter + incrementAmount
-        ball.innerHTML = ballDiameter
-        ball.style = `height: ${ballDiameter}px; width: ${ballDiameter}px; background-color: ${newColor};`
+        elBall.innerHTML = ballDiameter
+        elBall.style = `height: ${ballDiameter}px; width: ${ballDiameter}px; background-color: ${newColor};`
     }
-    ball.onclick()
+    elBall.onclick()
 }
