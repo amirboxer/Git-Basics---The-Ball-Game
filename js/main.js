817ff3d2beced6a8b61cf71ac5dc4e791817368d
amirboxer <amirboxer95@gmail.com>
2024-05-30 14:13:33 +0200

    Fix click function on ball

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 'use strict'
 
-function onBallClick() {
+function onBallClick(initialDiameter) {
     var ball = document.querySelector('.ball')
-    var ballDiameter = ball.clientHeight + 50 > 400 ? 100 : ball.clientHeight + 50
-    ball.innerHTML = ballDiameter
-    ball.style = `height: ${ballDiameter}px; width: ${ballDiameter}px;`
+    var ballDiameter = +initialDiameter
+    ball.onclick = () => {
+        ballDiameter = ballDiameter + 50 > 400 ? 100 : ballDiameter + 50
+        ball.innerHTML = ballDiameter
+        ball.style = `height: ${ballDiameter}px; width: ${ballDiameter}px;`
+    }
+    ball.onclick()
 }
