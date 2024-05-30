f1c1e581c1e268be0639e9ab90d76e622d2c0a21
amirboxer <amirboxer95@gmail.com>
2024-05-30 15:12:49 +0200

    Commit after reverse check

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 'use strict'
 
-function onBallClick(initialDiameter) {
-    var ball = document.querySelector('.ball')
-    var ballDiameter = +initialDiameter
-    ball.onclick = () => {
+function onBallClick(elBall, maxDiameter) {
+    var ballDiameter = +elBall.innerHTML
+    elBall.onclick = () => {
         var incrementAmount = getRandomInt(20, 60)
-        ballDiameter = ballDiameter + incrementAmount > 400 ? 100 : ballDiameter + incrementAmount
-        ball.innerHTML = ballDiameter
-        ball.style = `height: ${ballDiameter}px; width: ${ballDiameter}px;`
+        var newColor = getRandomColor()
+        ballDiameter = ballDiameter + incrementAmount > maxDiameter ? 100 : ballDiameter + incrementAmount
+        elBall.innerHTML = ballDiameter
+        elBall.style = `height: ${ballDiameter}px; width: ${ballDiameter}px; background-color: ${newColor};`
     }
-    ball.onclick()
+    elBall.onclick()
 }
--- js/util.js
+++ js/util.js
@@ -5,3 +5,9 @@ function getRandomInt(min, max) {
     const maxFloored = Math.floor(max);
     return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
 }
+
+function getRandomColor() {
+    return "#" + Math.floor(Math.random() * 16777215).toString(16)
+}
+
+console.log(getRandomColor())
