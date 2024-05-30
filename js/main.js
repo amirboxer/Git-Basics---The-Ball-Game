cbcb55f4cdaf48c0ad654c5fc7813e3f12fc53f3
amirboxer <amirboxer95@gmail.com>
2024-05-30 14:33:36 +0200

    Add getRandomColor to ball

--- js/main.js
+++ js/main.js
@@ -5,9 +5,10 @@ function onBallClick(initialDiameter) {
     var ballDiameter = +initialDiameter
     ball.onclick = () => {
         var incrementAmount = getRandomInt(20, 60)
+        var newColor = getRandomColor()
         ballDiameter = ballDiameter + incrementAmount > 400 ? 100 : ballDiameter + incrementAmount
         ball.innerHTML = ballDiameter
-        ball.style = `height: ${ballDiameter}px; width: ${ballDiameter}px;`
+        ball.style = `height: ${ballDiameter}px; width: ${ballDiameter}px; background-color: ${newColor};`
     }
     ball.onclick()
 }
